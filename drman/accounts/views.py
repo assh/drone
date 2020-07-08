b9f928c67f1f89e9939b810f09f84bebf239f512
Asish <asish.panda13@gmail.com>
2020-07-08 10:32:45 +0400

    Added Context in Views.py

--- drman/accounts/views.py
+++ drman/accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def home(request):
     return render(request,'accounts/dashboard.html')
 
 def about(request):
-    return render(request,'accounts/about.html')
+    dr = Drone.objects.all()
+    return render(request,'accounts/about.html',{'drones':dr})
 
 def customer(request):
     return render(request,'accounts/customer.html')
