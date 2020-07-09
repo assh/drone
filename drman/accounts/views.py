247054b51d5e6d01f4b5ce2da6d8c1ab276fe4be
Asish <asish.panda13@gmail.com>
2020-07-09 10:40:20 +0400

    Minor Fixes

--- drman/accounts/views.py
+++ drman/accounts/views.py
@@ -5,15 +5,15 @@ from .models import *
 
 def home(request):
     context = {
-        'customer': Customer.objects.all(), 
+        'customer': Customer.objects.all(),
         'mission': Mission.objects.all(),
-        'total_drone':Drone.objects.all().count(),
-        'maintenance_drone':Drone.objects.filter(status="m").count(),
+        'total_drone': Drone.objects.all().count(),
+        'maintenance_drone': Drone.objects.filter(status="m").count(),
         'mission_complete': Mission.objects.filter(mission_status="Complete").count(),
         'mission_pending': Mission.objects.filter(mission_status="Pending").count(),
         'mission_cancelled': Mission.objects.filter(mission_status="Cancelled").count(),
     }
-    return render(request, 'accounts/dashboard.html',context)
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def about(request):
