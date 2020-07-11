f28d9f4adbd05812d67726f88883b7987ef4e95a
Asish <asish.panda13@gmail.com>
2020-07-11 13:58:35 +0400

    Added Urls and made Mission Page

--- drman/accounts/urls.py
+++ drman/accounts/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home),
+    path('', views.home,name='main-home'),
     path('about/', views.about,name='drone'),
     path('customer/<str:pk>/', views.customer,name='customer'),
+    path('mission/',views.mission,name='all-mission'),
 ]
--- drman/accounts/views.py
+++ drman/accounts/views.py
@@ -4,9 +4,12 @@ from .models import *
 
 
 def home(request):
+    customer = Customer.objects.all()
+    mission = Mission.objects.all()
     context = {
-        'customer': Customer.objects.all(),
-        'mission': Mission.objects.all(),
+        'customer': customer,
+        #'mymission':mymission,
+        'mission': mission,
         'total_drone': Drone.objects.all().count(),
         'maintenance_drone': Drone.objects.filter(status="m").count(),
         'mission_complete': Mission.objects.filter(mission_status="Complete").count(),
@@ -27,3 +30,10 @@ def customer(request, pk):
     total_mission = mission.count()
     somedict = {'customer': customer, 'missions': mission,'total_mission':total_mission}
     return render(request, 'accounts/customer.html', somedict)
+
+def mission(request):
+    mission = Mission.objects.all()
+    context = {
+        'missions':mission,
+    }
+    return render(request,'accounts/allorder.html',context)
