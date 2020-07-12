d1e63d5ca06e37f94ed8bec863846969fe267c43
Asish <asish.panda13@gmail.com>
2020-07-12 17:04:32 +0400

    Client UI changes

--- drman/accounts/urls.py
+++ drman/accounts/urls.py
@@ -2,11 +2,12 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home,name='main-home'),
-    path('about/', views.about,name='drone'),
-    path('customer/<str:pk>/', views.customer,name='customer'),
-    path('mission/',views.mission,name='all-mission'),
-    path('launch/<str:pk>/',views.launch,name='launch'),
-    path('create_mission/', views.createMission,name='create-mission'),
-    path('update_mission/<str:pk>', views.updateMission,name='update-mission'),
+    path('', views.home, name='main-home'),
+    path('about/', views.about, name='drone'),
+    path('customer/<str:pk>/', views.customer, name='customer'),
+    path('statistics/', views.status, name='status'),
+    path('mission/', views.mission, name='all-mission'),
+    path('launch/<str:pk>/', views.launch, name='launch'),
+    path('create_mission/', views.createMission, name='create-mission'),
+    path('update_mission/<str:pk>', views.updateMission, name='update-mission'),
 ]
