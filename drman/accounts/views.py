62c201e3d66b155492e0a4992ad99fae483f3ca8
Asish <asish.panda13@gmail.com>
2020-07-15 09:54:13 +0400

    Added logout and username to navbar

--- drman/accounts/views.py
+++ drman/accounts/views.py
@@ -4,6 +4,10 @@ from .models import *
 from .forms import *
 from .filters import MissionFilter
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+
 
 def home(request):
     customer = Customer.objects.all()
@@ -134,11 +138,27 @@ def registerPage(request):
         form = CreateUser(request.POST)
         if form.is_valid():
             form.save()
-
-    context = {'form':form}
+            messages.success(request, 'Account was created')
+            return redirect('login')
+    context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main-home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
     context = {}
     return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
