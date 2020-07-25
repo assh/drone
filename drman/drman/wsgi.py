04318c62b8d76815c1eed2948357be14bf49fee3
Asish <asish.panda13@gmail.com>
2020-07-25 19:02:02 +0400

    hjk

--- drman/drman/wsgi.py
+++ drman/drman/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drman.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drman.drman.settings')
 
 application = get_wsgi_application()
