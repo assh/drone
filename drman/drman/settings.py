4b49e64d31424ae0eed586e88ba64972e6697f3f
Asish <asish.panda13@gmail.com>
2020-07-26 08:27:27 +0400

    changed secretkey

--- drman/drman/settings.py
+++ drman/drman/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%u!#90hghj1jee*yc)_ni-z9rgdw63y7xx=m6$(w8$04*ricrr'
-
+#SECRET_KEY = '%u!#90hghj1jee*yc)_ni-z9rgdw63y7xx=m6$(w8$04*ricrr'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
