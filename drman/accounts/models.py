7858510b8053fceb0aca3f00ecaf6c3494833096
Asish <asish.panda13@gmail.com>
2020-07-30 21:54:56 +0400

    model changes

--- drman/accounts/models.py
+++ drman/accounts/models.py
@@ -5,15 +5,38 @@ from django.contrib.auth.models import User
 
 class Customer(models.Model):
 
-    name = models.CharField(max_length=200, null=True)
-    phone = models.CharField(max_length=15, null=True)
+    customer_id = models.CharField(max_length=50, null=True)
+
+    LOAN_STATUS = (
+        ('a', 'Active'),
+        ('r', 'Registered'),
+        ('i', 'Inactive')
+    )
+
+    status = models.CharField(
+        max_length=1, choices=LOAN_STATUS, blank=True, default='a', null=True)
+    system_access = models.CharField(max_length=4, null=True)
+    zone = models.CharField(max_length=10, null=True)
+    first_name = models.CharField(max_length=20, null=True)
+    middle_name = models.CharField(max_length=20, null=True, blank=True)
+    last_name = models.CharField(max_length=20, null=True)
+    phone = models.CharField(max_length=20, null=True)
     email = models.CharField(max_length=200, null=True)
-    mobile = models.CharField(max_length=15, null=True)
-    address = models.CharField(max_length=200,null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    company_name = models.CharField(max_length=200, null=True)
+    mobile = models.CharField(max_length=15, null=True, blank=True)
+    line1 = models.CharField(max_length=50, null=True)
+    line2 = models.CharField(max_length=50, null=True)
+    line3 = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=15, null=True)
+    zip_code = models.CharField(max_length=20, null=True)
+    state = models.CharField(max_length=20, null=True)
+    country = models.CharField(max_length=50, null=True)
+    date_created = models.DateField(auto_now_add=True, null=True,blank=True)
+    date_start = models.DateField(auto_now_add=True, null=True,blank=True)
+    date_end = models.DateField(auto_now_add=True, null=True,blank=True)
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.customer_id}'
 
 
 class Location(models.Model):
@@ -39,16 +62,19 @@ class Drone(models.Model):
     status = models.CharField(
         max_length=1, choices=LOAN_STATUS, blank=True, default='m', null=True)
     locale = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
-    make = models.CharField(max_length=100,null=True,blank=True)
-    model_no = models.CharField(max_length=30,null=True,blank=True)
-    description = models.CharField(max_length=100,null=True,blank=True)
-    ean = models.CharField(max_length=10,null=True,blank=True)
-    price_currency = models.CharField(max_length=3,blank=True,null=True)
-    price = models.CharField(max_length=5,null=True,blank=False)
-    warranty = models.CharField(max_length=10,null=True,blank=True)
-    date_purchase = models.DateField(auto_now_add=False,auto_now=False,null=True,blank=True)
-    date_operation = models.DateField(auto_now_add=False,auto_now=False,null=True,blank=True)
-    date_shelved = models.DateField(auto_now_add=False,auto_now=False,null=True,blank=True)
+    make = models.CharField(max_length=100, null=True, blank=True)
+    model_no = models.CharField(max_length=30, null=True, blank=True)
+    description = models.CharField(max_length=100, null=True, blank=True)
+    ean = models.CharField(max_length=10, null=True, blank=True)
+    price_currency = models.CharField(max_length=3, blank=True, null=True)
+    price = models.CharField(max_length=5, null=True, blank=False)
+    warranty = models.CharField(max_length=10, null=True, blank=True)
+    date_purchase = models.DateField(
+        auto_now_add=False, auto_now=False, null=True, blank=True)
+    date_operation = models.DateField(
+        auto_now_add=False, auto_now=False, null=True, blank=True)
+    date_shelved = models.DateField(
+        auto_now_add=False, auto_now=False, null=True, blank=True)
 
     def __str__(self):
         return self.droneid
@@ -66,11 +92,12 @@ class Mission(models.Model):
             np = f'M{no:06}'
             return np
 
-    mission_id = models.CharField(max_length=10, null=True,default = incrementid,editable=False)
+    mission_id = models.CharField(
+        max_length=10, null=True, default=incrementid, editable=False)
     date_future = models.DateTimeField(auto_now_add=False, null=True)
     drone = models.ForeignKey(Drone, null=True, on_delete=models.SET_NULL)
     state = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
-    manager = models.ForeignKey(User,null=True,on_delete=models.SET_NULL)
+    manager = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     customer = models.ForeignKey(
         Customer, null=True, on_delete=models.SET_NULL)
     MISSION_TYPE = (
@@ -97,21 +124,23 @@ class Mission(models.Model):
     mission_status = models.CharField(
         max_length=10, choices=STATE_TYPE, default='Pending', blank=True, null=True)
 
-    LAUNCH_MODE ={
-        ('AUTO','AUTO'),
-        ('MANUAL','MANUAL')
+    LAUNCH_MODE = {
+        ('AUTO', 'AUTO'),
+        ('MANUAL', 'MANUAL')
     }
-    launch_mode = models.CharField(max_length=7,choices=LAUNCH_MODE,default='MANUAL',null=True)
-    mission_pic = models.ImageField(null=True,blank=True,default="logo.png")
-    launch_now = models.BooleanField(null=True,default=False)
+    launch_mode = models.CharField(
+        max_length=7, choices=LAUNCH_MODE, default='MANUAL', null=True)
+    mission_pic = models.ImageField(null=True, blank=True, default="logo.png")
+    launch_now = models.BooleanField(null=True, default=False)
 
     def __str__(self):
         return str(self.mission_id)
 
+
 class Launch(models.Model):
 
-    mission = models.CharField(max_length=10,null=True)
-    now = models.CharField(max_length=1,default='1',null=True)
+    mission = models.CharField(max_length=10, null=True)
+    now = models.CharField(max_length=1, default='1', null=True)
 
     def __str__(self):
         return self.mission
