3bd8f5d5d402650c5dcd2bed553c86b5f4fff897
Asish <asish.panda13@gmail.com>
2020-07-31 19:54:41 +0400

    Password reset functionality

--- drman/accounts/urls.py
+++ drman/accounts/urls.py
@@ -24,12 +24,12 @@ urlpatterns = [
     path('my_drone/<str:pk>', views.my_drone, name='my-drone'),
     path('launch_drone/<str:pk>', views.launch_drone, name='launch-drone'),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
          name='reset_password'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_sent.html'),
          name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_form.html'),
          name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'),
          name='password_reset_complete'),
 ]
