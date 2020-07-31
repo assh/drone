9c26b4fd165f0833c1abb36a98cf5563f6e3a95f
Asish <asish.panda13@gmail.com>
2020-07-31 19:31:48 +0400

    Added password reset support

--- drman/accounts/urls.py
+++ drman/accounts/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
 
     path('reset_password/', auth_views.PasswordResetView.as_view(),
          name='reset_password'),
-    path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),
          name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
